36f023e38380c7e50fe02dbe83891533ffbe6831
stefian <stefan.ianta@w3ai.net>
2020-07-30 13:49:02 -0300

    + Blockchain.prototype.hashBlock = function (previousBlockHash, currentBlockData, nonce) {

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -37,8 +37,10 @@ Blockchain.prototype.createNewTransaction = function (amount, sender, recipient)
   return this.getLastBlock()['index'] + 1;  // Return the nr / index of the new block containing the tx
 }
 
-Blockchain.prototype.hashBlock = function (blockData) { // Hashing block data into a fixed lenght string
-
+Blockchain.prototype.hashBlock = function (previousBlockHash, currentBlockData, nonce) { // Hashing block data into a fixed lenght string
+  const dataAsString = previousBlockHash + nonce.toString + JSON.stringify(currentBlockData);
+  const hash = sha256(dataAsString);
+  return hash;
 }
 
 
