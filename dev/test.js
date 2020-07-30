e3280a121350bb3d204431db44957b6bb3f91499
stefian <stefan.ianta@w3ai.net>
2020-07-30 11:42:02 -0300

    + // Test createNewTransaction() - expect a bitcoin Blockchain with 1 block and 1 pending tx

--- dev/test.js
+++ dev/test.js
@@ -2,8 +2,15 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389, 'JHGCJHGCKHVKHGV', 'HJVKCKGHCKGH')
-bitcoin.createNewBlock(111, 'JHGKBKJHBBJJHK', 'HJVNGRSGGMHGKGH')
-bitcoin.createNewBlock(2899, 'GKUYGKUYKYBKV', 'JKHVJGFDFFSGRSE')
+// Test createNewTransaction() - expect a bitcoin Blockchain with 1 block and 1 pending tx
+bitcoin.createNewBlock(2389, 'JHGCJHGCKHVKHGV', 'HJVKCKGHCKGH');
+
+bitcoin.createNewTransaction(100, 'ALEXYTF865F65UUF', 'JEN776GFU56F65F4')
+
+
+// Test createNewBlock() - expect a bitcoin blocchain w 3 blocks
+// bitcoin.createNewBlock(2389, 'JHGCJHGCKHVKHGV', 'HJVKCKGHCKGH');
+// bitcoin.createNewBlock(111, 'JHGKBKJHBBJJHK', 'HJVNGRSGGMHGKGH');
+// bitcoin.createNewBlock(2899, 'GKUYGKUYKYBKV', 'JKHVJGFDFFSGRSE');
 
 console.log(bitcoin);
