725c11f2c790528ff3073efd67460f6ec28fa359
stefian <stefan.ianta@w3ai.net>
2020-07-30 11:50:49 -0300

    + // Equivalent to mining a block after creating a new tx, to include the tx

--- dev/test.js
+++ dev/test.js
@@ -7,10 +7,12 @@ bitcoin.createNewBlock(2389, 'JHGCJHGCKHVKHGV', 'HJVKCKGHCKGH');
 
 bitcoin.createNewTransaction(100, 'ALEXYTF865F65UUF', 'JEN776GFU56F65F4')
 
+// Equivalent to mining a block after creating a new tx, to include the tx
+bitcoin.createNewBlock(123123, 'JHGKBKJHBBJJHK', 'HJVNGRSGGMHGKGH');
 
 // Test createNewBlock() - expect a bitcoin blocchain w 3 blocks
 // bitcoin.createNewBlock(2389, 'JHGCJHGCKHVKHGV', 'HJVKCKGHCKGH');
 // bitcoin.createNewBlock(111, 'JHGKBKJHBBJJHK', 'HJVNGRSGGMHGKGH');
 // bitcoin.createNewBlock(2899, 'GKUYGKUYKYBKV', 'JKHVJGFDFFSGRSE');
 
-console.log(bitcoin);
+console.log(bitcoin.chain[1]);
