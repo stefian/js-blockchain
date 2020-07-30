ab3682be5d054a5ee9d69d849a1f458928c930b0
stefian <stefan.ianta@w3ai.net>
2020-07-30 08:12:28 -0300

    + Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -2,3 +2,19 @@ function Blockchain() {
   this.chain = [];
   this.newTransactions = [];
 }
+
+Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
+  const newBlock = {
+    index: this.chain.length + 1, // block number
+    timestamp: Date.now(),
+    transactions: this.newTransactions,
+    nonce: nonce, // Proof of work - a number - proof that we created the block in a legitimate way
+    hash: hash, // data from our new block - all new tx compressed in a single string
+    previousBlockHash: previousBlockHash  // the data hashed of the previous block
+  };
+
+  this.newTransactions = [];
+  this.chain.push(newBlock);
+
+  return newBlock;
+}
