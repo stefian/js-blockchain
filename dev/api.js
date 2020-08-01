90645be6da92549e91ccfb29fcdfaa9abd5af36e
stefian <stefan.ianta@w3ai.net>
2020-08-01 06:34:57 -0300

    + const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);

--- dev/api.js
+++ dev/api.js
@@ -16,8 +16,8 @@ app.get('/blockchain', function (req, res) {
 
 // create a new transaction
 app.post('/transaction', function (req, res) {
-  console.log(req.body);
-  res.send(`The amount of tx is ${req.body.amount} bitcoin`);
+  const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  res.json({ note: `Transaction will be added in block ${blockIndex}` });
 });
 
 // mine a new block
