7d9e4fe990097d579886314a0077e6c165e99ea6
stefian <v_ianta@yahoo.com>
2020-08-01 13:28:01 -0300

    + const currentNodeUrl = process.argv[3];

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,8 +1,11 @@
 const sha256 = require('sha256');
+const currentNodeUrl = process.argv[3];
 
 function Blockchain() {
   this.chain = [];
   this.pendingTransactions = [];
+
+  this.currentNodeUrl = currentNodeUrl;
 
   this.createNewBlock(100, '0', '0'); // Genesis / inital block
   // For Cogito - Genesis block should be related to the project / owner /budget info
