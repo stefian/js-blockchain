945fb6231402e9a39b74f260aa63579adedc4258
stefian <v_ianta@yahoo.com>
2020-08-07 07:03:35 -0300

    + // Validating the Genesis Block // in Cogito to validate the Project def, budget, status, tests/requirements

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -74,7 +74,27 @@ Blockchain.prototype.proofOfWork = function (previousBlockHash, currentBlockData
 }
 
 Blockchain.prototype.chainIsValid = function (blockchain) { // will return a boolean if blockchain is valid or not
+  let validChain = true;
   
+  // iterate through all blocks and check the hashes
+  for (var i = 1; i < blockchain.length; i++) {
+    const currentBlock = blockchain[i];
+    const prevBlock = blockchain[i - 1];
+    const blockHash = this.hashBlock(prevBlock['hash'], { transactions: currentBlock['transactions'], index: currentBlock['index'] }, currentBlock['nonce']);
+    if (blockHash.substring(0, 4) !== '0000') validChain = false;
+    if (currentBlock['previousBlockHash'] !== prevBlock['hash']) validChain = false;  // chain not valid
+  };
+
+  // Validating the Genesis Block // in Cogito to validate the Project def, budget, status, tests/requirements
+  const genesisBlock = blockchain[0];
+  const correctNonce = genesisBlock['nonce'] === 100;
+  const correctPreviousBlockHash = genesisBlock['previousBlockHash'] === '0';
+  const correctHash = genesisBlock['hash'] === '0';
+  const correctTransactions = genesisBlock['transactions'].length === 0;
+
+  if (!correctNonce || !correctPreviousBlockHash || !correctHash || !correctTransactions) validChain = false;
+
+  return validChain;
 };
 
 module.exports = Blockchain;
